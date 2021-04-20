b5aecee110e474f72409b139dc51f10d9ae0a783
UglyNixon <58419699+UglyNixon@users.noreply.github.com>
2021-04-20 10:26:26 +0300

    Delete learnPQ directory

--- learnPQ/client/src/utils/consts.js
+++
@@ -1,6 +0,0 @@
-export const ADMIN_ROUTE = '/admin'
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const BASKET_ROUTE = '/basket'
-export const DEVIVE_ROUTE = '/device'
-export const SHOP_ROUTE = '/'
